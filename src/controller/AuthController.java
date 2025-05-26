91f05c1623d9b92febace46d88d5ee363b1683ef
Charls <carlosriver041105@gmail.com>
2025-05-25 23:25:22 -0600

    Ajustes en el endpoit /login

--- src/controller/AuthController.java
+++ src/controller/AuthController.java
@@ -1,14 +1,16 @@
 package controller;
+import java.io.IOException;
 
 import org.json.JSONObject;
 import http.HttpRequest;
 import http.HttpResponse;
+import model.User;
 import service.AuthService;
 
 //Clase encargada de conectarse a AuthService
 public class AuthController {
     
-    public static void register(HttpRequest request, HttpResponse response) {
+    public static void register(HttpRequest request, HttpResponse response) throws IOException{
         try {
             JSONObject json = new JSONObject(request.getBody());
             String username = json.getString("username");
@@ -29,8 +31,34 @@ public class AuthController {
 
         }catch(Exception e) {
             response.setStatus(500);
-            //response.sendJson("{\"Error\":\"Internal error into Server\"}");
+            response.sendJson("{\"Error\":\"Internal error into Server\"}");
         }
+    }
+
+    public static void login(HttpRequest request, HttpResponse response) throws IOException {
+        
+        try {
+            JSONObject body = new JSONObject(request.getBody());
+
+            String username = body.optString("usernmae");
+            String password = body.optString("password");
+
+            AuthService service = new AuthService();
+            boolean success = service.login(username, password);
+
+            if (success) {
+                response.setStatus(200);
+                response.sendJson("{\"message\":\"Welcome\"}");
+            } else {
+                response.setStatus(400);
+                response.sendJson("{\"error\":\"The user do not exist or credentials invalaible\"}");
+            }
+
+        } catch(Exception e) {
+            response.setStatus(500);
+            response.sendJson("{\"Error\":\"Internal error into Server\"}");
+        }
+ 
     }
 
 }
--- src/http/Router.java
+++ src/http/Router.java
@@ -6,7 +6,15 @@ import controller.AuthController;
 //Aqui se define la logica para cada ruta, y
 //delega,ps al controlador
 public class Router {
-    
+    //Paths
+    private static final String PATH_ROOT = "/";
+    private static final String PATH_REGISTER = "/register";
+    private static final String PATH_LOGIN = "/login";
+
+    //Methods
+    private static final String METHOD_GET = "GET";
+    private static final String METHOD_POST = "POST";
+
     //Establecer el metodo Router para comunicar
     //con HttpRequest y HttpResponse
     public static void route(HttpRequest request, HttpResponse response) throws IOException {
@@ -15,22 +23,40 @@ public class Router {
         String method = request.getMethod();
         String path = request.getPath();
 
-        //test
-        System.out.println("Method: "+method+", Path: "+path);
-
-        if(path.equals("/register") && method.equals("POST")) {
-            AuthController.register(request, response);
-        } else if(path.equals("/register") && method.equals("GET")) {
-            response.setStatus(200);
-            response.sendJson("{\"info\":\"Usa POST para registrar un usuario.\"}");
-        }else if(path.equals("/") && method.equals("GET")) {
-            response.setStatus(200);
-            response.sendJson("{\"messege\":\"Server is ready\"}");
+        if (METHOD_GET.equals(method)) {
+            switch (path) {
+                case PATH_ROOT:
+                     response.setStatus(200);
+                    response.sendJson("{\"message\":\"Server is ready\"}");
+                    break;
+                case PATH_REGISTER:
+                    response.setStatus(200);
+                    response.sendJson("{\"info\":\"Usa POST para registrar un usuario.\"}");
+                    break;
+                default:
+                    sendNotFound(response);
+            }
+        } else if (METHOD_POST.equals(method)) {
+            switch (path) {
+                case PATH_REGISTER:
+                    AuthController.register(request, response);
+                    break;
+                case PATH_LOGIN:
+                    AuthController.login(request, response);
+                    break;
+            
+                default:
+                    sendNotFound(response);
+            }
         } else {
-            response.setStatus(404);
-            response.sendJson("{\"error\":\"Route not foud\"}");
+            sendNotFound(response);
         }
 
+    }
+
+    private static void sendNotFound(HttpResponse response) throws IOException {
+        response.setStatus(404);
+        response.sendJson("{\"error\":\"Route not found\"}");
     }
 
 }
